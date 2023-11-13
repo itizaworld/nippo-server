6cb0a4f2034cfb8e30c4eaf77644c101dcba1153
itizawa <itizawa.pen@gmail.com>
2023-11-13 22:20:27 +0900

    Add endpoint to fetch user objectives

--- src/presentations/controllers/objective.ts
+++ src/presentations/controllers/objective.ts
@@ -1,8 +1,11 @@
 import * as express from "express";
 import { User } from "~/models/User";
 import { CreateObjectiveUseCase } from "~/useCases/Objective/CreateObjectiveUseCase";
+import { FetchUserObjectivesUseCase } from "~/useCases/Objective/FetchUserObjectivesUseCase";
 
 const createObjectiveUseCase = new CreateObjectiveUseCase();
+const fetchUserObjectivesUseCase = new FetchUserObjectivesUseCase();
+
 export const setupObjectivesRoutes = (express: express.Express): void => {
   express.post(
     "/api/objectives",
@@ -16,6 +19,24 @@ export const setupObjectivesRoutes = (express: express.Express): void => {
           description: req.body.description,
         });
         return res.status(200).json({ object: createdObject });
+      } catch (error) {
+        return res
+          .status(503)
+          .send({ message: "予期せぬエラーが発生しました" });
+      }
+    },
+  );
+
+  express.get(
+    "/api/objectives/me",
+    async (req: express.Request & { user: User }, res: express.Response) => {
+      const { user } = req;
+
+      try {
+        const objects = await fetchUserObjectivesUseCase.execute({
+          userId: user._id,
+        });
+        return res.status(200).json({ objects });
       } catch (error) {
         return res
           .status(503)
