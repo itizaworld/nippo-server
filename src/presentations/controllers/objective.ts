83ad34aa03f2dc6d2fbe0e488ca5eff402c1f9d1
itizawa <itizawa.pen@gmail.com>
2023-11-24 21:12:51 +0900

    単体取得のエンドポイントを実装した

--- src/presentations/controllers/objective.ts
+++ src/presentations/controllers/objective.ts
@@ -1,10 +1,12 @@
 import * as express from "express";
 import { User } from "~/models/User";
 import { CreateObjectiveUseCase } from "~/useCases/Objective/CreateObjectiveUseCase";
+import { RetrieveObjectiveUseCase } from "~/useCases/Objective/RetrieveObjectiveUseCase";
 import { FetchUserObjectivesUseCase } from "~/useCases/Objective/FetchUserObjectivesUseCase";
 
 const createObjectiveUseCase = new CreateObjectiveUseCase();
 const fetchUserObjectivesUseCase = new FetchUserObjectivesUseCase();
+const retrieveObjectiveUseCase = new RetrieveObjectiveUseCase();
 
 export const setupObjectivesRoutes = (express: express.Express): void => {
   express.post(
@@ -35,6 +37,29 @@ export const setupObjectivesRoutes = (express: express.Express): void => {
       try {
         const objects = await fetchUserObjectivesUseCase.execute({
           userId: user._id,
+        });
+        return res.status(200).json({ objects });
+      } catch (error) {
+        return res
+          .status(503)
+          .send({ message: "予期せぬエラーが発生しました" });
+      }
+    },
+  );
+
+  express.get(
+    "/api/objectives/:id",
+    async (
+      req: express.Request<object, object, object, { id: string }> & {
+        user: User;
+      },
+      res: express.Response,
+    ) => {
+      const { id } = req.query;
+
+      try {
+        const objects = await retrieveObjectiveUseCase.execute({
+          _id: id,
         });
         return res.status(200).json({ objects });
       } catch (error) {
---
+++ src/useCases/Objective/RetrieveObjectiveUseCase/RetrieveObjectiveUseCase.ts
@@ -0,0 +1,9 @@
+import { Objective, ObjectiveModel } from "~/models/Objective";
+
+export class RetrieveObjectiveUseCase {
+  async execute({ _id }: { _id: string }): Promise<Objective[]> {
+    return await ObjectiveModel.find({
+      _id,
+    });
+  }
+}
