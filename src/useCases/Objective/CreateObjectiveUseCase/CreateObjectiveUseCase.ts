0a0fd0218077192b0dafc53fd5373f2d8bd59ad4
itizawa <itizawa.pen@gmail.com>
2023-11-27 22:44:40 +0900

    slug をdefault値に設定した

--- src/useCases/Objective/CreateObjectiveUseCase/CreateObjectiveUseCase.ts
+++ src/useCases/Objective/CreateObjectiveUseCase/CreateObjectiveUseCase.ts
@@ -1,3 +1,4 @@
+import { Types } from "mongoose";
 import { Objective, ObjectiveModel } from "~/models/Objective";
 import { User } from "~/models/User";
 
@@ -9,9 +10,12 @@ export class CreateObjectiveUseCase {
   }: Pick<Objective, "name" | "description"> & {
     currentUser: User;
   }): Promise<Objective> {
+    const objectiveId = new Types.ObjectId();
     return await ObjectiveModel.create({
+      _id: objectiveId,
       name,
       description,
+      slug: objectiveId,
       createdUserId: currentUser._id,
       status: "INPROGRESS",
     });
