f505223f1cdba41e9404e5510cdf776e8365024a
itizawa <itizawa.pen@gmail.com>
2023-12-20 22:42:51 +0900

    objectiveId に紐づいた task 一覧を取得するメソッドを UseCase に実装した

--- src/useCases/TaskUseCase/TaskUseCase.ts
+++ src/useCases/TaskUseCase/TaskUseCase.ts
@@ -2,6 +2,20 @@ import { Task, TaskModel } from "~/models/Task";
 import { User } from "~/models/User";
 
 export class TaskUseCase {
+  async list({
+    objectiveId,
+    page,
+    limit,
+  }: {
+    objectiveId: string;
+    page: number;
+    limit: number;
+  }): Promise<Task[]> {
+    return await TaskModel.find({ objectiveId })
+      .sort({ date: -1 })
+      .skip((page - 1) * limit)
+      .limit(limit);
+  }
   async create({
     currentUser,
     title,
